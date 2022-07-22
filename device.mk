bc41d91ab1ffa7d50a08e7a4c49401eb1901856c
AnierinB <anierin@evolution-x.org>
2022-07-22 22:51:27 +0000

    instantnoodle: Configure OPlusExtras device specific features
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- device.mk
+++ device.mk
@@ -25,6 +25,7 @@ PRODUCT_PACKAGES += \
     EvolutionSettingsResTarget \
     EvolutionSystemUIResTarget \
     OPlusFrameworksResTarget \
+    OPlusExtrasResTarget \
     OPlusSettingsProviderResTarget \
     OPlusSystemUIResTarget
 
